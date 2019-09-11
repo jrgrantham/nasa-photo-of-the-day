38c3fbd2bb061fdc1aeb65eb3bf6352cbb011d59
James Grantham <james.r.grantham@gmail.com>
2019-09-11 13:10:52 +0100

    UseEffect and axious to get data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from 'axios';
 import "./App.css";
 
 function App() {
+
+  useEffect(() => {
+    axios.get('https://lambda-github-api-server.herokuapp.com/')
+      .then(response => {
+        console.log(response)
+      })
+    }
+  , [])
+
   return (
     <div className="App">
+      <div>
+        here goes the TITLE
+      </div>
+      <div>
+        here goes the DATE
+      </div>
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        app! Have fun!
       </p>
     </div>
   );
